57f8fbb57f08578193e139e5259a91ec5f24b847
sasha_bu <sasha_bu@hotmail.com>
2011-06-08 08:19:55 +0000

    Added a centralized content manager so that each image file is read once instead of O(rows*columns) times. This should also reduce memory usage significantly.
    Each cell in the JPanelBlockDisplay is a DrawImage object, and before the content manager was centralized, each DrawImage object loaded a copy of every image file (including the large welcome screen and all the power-up images).
    
    I also removed some unused code from JPanelBlockDisplay.

--- BattleTetris/src/DrawImage.java
+++ BattleTetris/src/DrawImage.java
@@ -10,21 +10,10 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class DrawImage extends JPanel {
 
-	private Image[] imgs = null;
 	private int current = 0;
 
-	public DrawImage()
-	{
-		imgs = new Image[PowerUp.NUM_POWERUPS];
-		for (int i=1; i <= PowerUp.NUM_POWERUPS; i++)
-		{
-			Toolkit toolkit = getToolkit();
-			// create the image using the toolkit
-			Image img = toolkit.createImage(PowerUp.getImageName(i));
-
-			imgs[i-1] = img;
-		}
-	}
+	public DrawImage() { }
+	
 	public void setImage(int pwrType)
 	{
 		current = pwrType;
@@ -40,9 +29,10 @@ public class DrawImage extends JPanel {
 			// the internal margins of the component
 			Insets i = getInsets();
 			// draw to fill the entire component
-			g.drawImage(imgs[current-1], i.left, i.top, d.width - i.left - i.right,
-			            d.height - i.top - i.bottom, this );
-
+			g.drawImage(ContentManager.getImage(current),
+			            i.left, i.top,
+			            d.width - i.left - i.right, d.height - i.top - i.bottom,
+			            this );
 		}
 	}
 }
